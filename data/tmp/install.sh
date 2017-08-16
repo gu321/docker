1c4c3e511e76fc4f3af5ec49eb8eb18ad4349a7c
xpure <xpure@foxmail.com>
2017-08-16 09:31:20 +0800

    .

--- data/tmp/install.sh
+++ data/tmp/install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+sudo wget http://master.dl.sourceforge.net/project/d-apt/files/d-apt.list -O /etc/apt/sources.list.d/d-apt.list
+wget -qO - https://dlang.org/d-keyring.gpg | sudo apt-key add -
+sudo apt-get update -y
+sudo apt-get install dmd-bin -y
+
 function INIT_USER {
 groupadd -g $2 $1
 useradd -u $2 -g $2 -s /bin/bash -m $1
